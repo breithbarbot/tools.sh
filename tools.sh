e7c28a3aba4901865ffe02161f634c296d9ac803
Breith Barbot <b.breith@gmail.com>
2017-03-15 11:55:53 +0100

    Update script tools.sh

--- tools.sh
+++ tools.sh
@@ -9,12 +9,12 @@
 PS3='Selected : '
 
 # List of available choices :
-LISTE=( "[1] Reset (with cache)"
-        "[2] Reset (without cache)"
-        "[3] Clean cache"
-        "[4] Clean cache (with the rm command and the -Rf argument)"
-        "[5] Update projet (Composer + Bower + BDD)"
-        "[6] Remove media files" )
+LISTE=( "Reset (with cache)"
+        "Reset (without cache)"
+        "Clean cache"
+        "Clean cache (with the rm command and the -Rf argument)"
+        "Update projet (Composer + Bower + BDD)"
+        "Remove media files" )
 
 # Choices available
 select CHOIX in "${LISTE[@]}" ; do
@@ -31,9 +31,9 @@ select CHOIX in "${LISTE[@]}" ; do
 
         php bin/console cache:clear
         php bin/console cache:clear --env=prod --no-debug
-        echo "-----------------------"
-        echo "Reset (with cache) [OK]"
-        echo "-----------------------"
+        echo -e "\e[0m \e[1;32;40m -----------------------\e[0m"
+        echo -e "\e[0m \e[1;32;40m [OK] Reset (with cache) \e[0m"
+        echo -e "\e[0m \e[1;32;40m -----------------------\e[0m"
         break
         ;;
 
@@ -46,9 +46,9 @@ select CHOIX in "${LISTE[@]}" ; do
         php bin/console doctrine:database:create
         php bin/console doctrine:schema:update --force
         php bin/console doctrine:fixtures:load
-        echo "--------------------------"
-        echo "Reset (without cache) [OK]"
-        echo "--------------------------"
+        echo -e "\e[0m \e[1;32;40m --------------------------\e[0m"
+        echo -e "\e[0m \e[1;32;40m [OK] Reset (without cache)"
+        echo -e "\e[0m \e[1;32;40m --------------------------\e[0m"
         break
         ;;
 
@@ -59,26 +59,26 @@ select CHOIX in "${LISTE[@]}" ; do
         echo "-----------------"
         php bin/console cache:clear
         php bin/console cache:clear --env=prod --no-debug
-        echo "----------------"
-        echo "Clean cache [OK]"
-        echo "----------------"
+        echo -e "\e[0m \e[1;32;40m ----------------\e[0m"
+        echo -e "\e[0m \e[1;32;40m [OK] Clean cache"
+        echo -e "\e[0m \e[1;32;40m ----------------\e[0m"
         break
         ;;
 
         4)
         echo ""
-        echo "-----------------------------------------"
+        echo "-----------------------------------------------------------"
         echo "Start clean cache (with the rm command and the -Rf argument)"
-        echo "-----------------------------------------"
+        echo "-----------------------------------------------------------"
         rm -Rf var/cache/*
         rm -Rf var/sessions/*
         rm -Rf var/logs/*
 
         php bin/console cache:clear
         php bin/console cache:clear --env=prod --no-debug
-        echo "----------------------------------------"
-        echo "Clean cache (with the rm command and the -Rf argument) [OK]"
-        echo "----------------------------------------"
+        echo -e "\e[0m \e[1;32;40m -----------------------------------------------------------\e[0m"
+        echo -e "\e[0m \e[1;32;40m [OK] Clean cache (with the rm command and the -Rf argument)"
+        echo -e "\e[0m \e[1;32;40m -----------------------------------------------------------\e[0m"
         break
         ;;
 
@@ -93,9 +93,9 @@ select CHOIX in "${LISTE[@]}" ; do
 
         php bin/console cache:clear
         php bin/console cache:clear --env=prod --no-debug
-        echo "-------------------------------------------"
-        echo "Update projet (Composer + Bower + BDD) [OK]"
-        echo "-------------------------------------------"
+        echo -e "\e[0m \e[1;32;40m -------------------------------------------\e[0m"
+        echo -e "\e[0m \e[1;32;40m [OK] Update projet (Composer + Bower + BDD)"
+        echo -e "\e[0m \e[1;32;40m -------------------------------------------\e[0m"
         break
         ;;
 
@@ -108,19 +108,10 @@ select CHOIX in "${LISTE[@]}" ; do
         rm -Rf web/uploads/users/avatars/*
         rm -Rf web/uploads/wysiwyg/source/*
         rm -Rf web/uploads/wysiwyg/thumbs
-        echo "-----------------------"
-        echo "Remove media files [OK]"
-        echo "-----------------------"
+        echo -e "\e[0m \e[1;32;40m -----------------------\e[0m"
+        echo -e "\e[0m \e[1;32;40m [OK] Remove media files"
+        echo -e "\e[0m \e[1;32;40m -----------------------\e[0m"
         break
         ;;
     esac
 done
-
-echo ""
-echo ""
-echo "********************************"
-echo "********************************"
-echo "Close in less than 5 secondes..."
-echo "********************************"
-echo "********************************"
-sleep 5
