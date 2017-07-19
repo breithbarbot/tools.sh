33e0b4372124da9cd1c14a19ece51b60904a4ba9
Corentin Dehay <corentin.dehay@viacesi.fr>
2017-07-19 17:57:09 +0200

    Update tools.sh
    remove unnecessary comma after echo

--- tools.sh
+++ tools.sh
@@ -17,7 +17,7 @@ if [[ "$OSTYPE" == linux* ]]; then
     USER="`whoami`"
     if [[ "$USER" == "root" ]]; then
         echo -e '\033[1;31m----------------------------------------------------\x1b[m'
-        echo -e "\033[1;31mAttention ! Vous executez le script en tant que root\x1b[m";
+        echo -e "\033[1;31mAttention ! Vous executez le script en tant que root\x1b[m"
 	echo -e '\033[1;31m----------------------------------------------------\x1b[m'
 	exit 1
     fi
